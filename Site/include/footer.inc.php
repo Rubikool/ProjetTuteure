fe79cdbad0b775721c47de7183598f2ffc1fe9d1
BenjaminLacroix <lacroix_benjamin@orange.fr>
2016-01-11 10:25:54 +0100

    AjoutMéthode

--- Site/include/footer.inc.php
+++ Site/include/footer.inc.php
@@ -1,8 +1,6 @@
 
 <div id="footer">
-Le bétisier de l'IUT à votre service, depuis novembre 2013
-<br />
-    © IUT du Limousin  DUT Informatique année 2 
+
 
 </div>
 </body>
--- Site/include/functions.inc.php
+++ Site/include/functions.inc.php
@@ -4,4 +4,5 @@
 		$date = $membres[2].'-'.$membres[0].'-'.$membres[1];
 		return $date;
 	}
+
 ?>
