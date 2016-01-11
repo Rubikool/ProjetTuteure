5c45f24ff39df679a4ef9837f9316cde473300f9
AlexPlt <alex1000.poireault@orange.fr>
2016-01-11 09:02:14 +0100

    RemiseANiveau
    toutes la gestion des eleves

--- Site/include/menu.inc.php
+++ Site/include/menu.inc.php
@@ -16,7 +16,6 @@ if (empty($_SESSION["connecte"]) OR $_SESSION["connecte"] != 1){
 	<li><a href="index.php?page=0">Personne</a>
 		<ul>
 			<li><a href="index.php?page=6">Lister</a></li>
-			<li><a href="index.php?page=5">Ajouter</a></li>
 			<li><a href="index.php?page=7">Supprimer</a></li>
 		</ul>
 	</li>
@@ -40,13 +39,6 @@ if (empty($_SESSION["connecte"]) OR $_SESSION["connecte"] != 1){
 		<ul>
 			<li><a href="index.php?page=9">Ajouter</a></li>
 			<li><a href="index.php?page=0">Charger</a></li>
-		</ul>
-	</li>
-	<li><a href="index.php?page=0">Nouvelle configuration</a></li>
-	<li><a href="index.php?page=0">Mouvement</a>
-		<ul>
-			<li><a href="index.php?page=0">Creer</a></li>
-			<li><a href="index.php?page=0">Lister</a></li> <!-- méthodes Utiliser et Modifier -->
 		</ul>
 	</li>
 </ul>
--- Site/include/pages/accueil.inc.php
+++ Site/include/pages/accueil.inc.php
@@ -4,22 +4,22 @@ $manager = new CubeManager($bd);
 ?>
 
 <div class="choixRCAccueil">
-  <a href="index.php?page=3&cube=2" ><img src="image/2x2x2.png" class="imageRCAccueil" /></a>
+  <a href="index.php?page=4&cube=2" ><img src="image/2x2x2.png" class="imageRCAccueil" /></a>
 </div>
 <div class="choixRCAccueil">
-  <a href="index.php?page=3&cube=3" ><img src="image/3x3x3.png" class="imageRCAccueil" /></a>
+  <a href="index.php?page=4&cube=3" ><img src="image/3x3x3.png" class="imageRCAccueil" /></a>
 </div>
 <div class="choixRCAccueil">
-  <a href="index.php?page=3&cube=4" ><img src="image/4x4x4.png" class="imageRCAccueil" /></a>
+  <a href="index.php?page=4&cube=4" ><img src="image/4x4x4.png" class="imageRCAccueil" /></a>
 </div>
 <br/>
 <div class="choixRCAccueil">
-  <a href="index.php?page=3&cube=5" ><img src="image/5x5x5.png" class="imageRCAccueil" /></a>
+  <a href="index.php?page=4&cube=5" ><img src="image/5x5x5.png" class="imageRCAccueil" /></a>
 </div>
 <div class="choixRCAccueil">
-  <a href="index.php?page=3&cube=6" ><img src="image/6x6x6.png" class="imageRCAccueil" /></a>
+  <a href="index.php?page=4&cube=6" ><img src="image/6x6x6.png" class="imageRCAccueil" /></a>
 </div>
 <div class="choixRCAccueil">
-  <a href="index.php?page=3&cube=7" ><img src="image/7x7x7.png" class="imageRCAccueil" /></a>
+  <a href="index.php?page=4&cube=7" ><img src="image/7x7x7.png" class="imageRCAccueil" /></a>
 </div>
 <br/>
