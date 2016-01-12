76316a4899ccda25bf8edf2c5861147de0f36b22
AlexPlt <alex1000.poireault@orange.fr>
2016-01-12 09:56:02 +0100

    ficherOK

--- Site/include/pages/ChapitreFichier.inc.php
+++ Site/include/pages/ChapitreFichier.inc.php
@@ -1,5 +1,56 @@
-<form method="post" action="#">
-<label for="mon_fichier">Fichier (format PDF | max. 1 Mo) :</label><br />
-       <input type="submit" name="ajoutFichier" value="Ajouter un fichier" />
-<input type="submit" name="submit" value="Envoyer" />
+<?php
+$maxsize = 1000000;
+$extensions_valides = array('pdf');
+$nomRepertoire = 'fichierMethode/';
+$taille = 0;
+$extension = 0;
+$erreur = 0;
+
+if(empty($_FILES)){
+?>
+<form method="post" action="#" enctype="multipart/form-data">
+
+     <label for="mon_fichier">Fichier (tous formats | max. 1 Mo) :</label><br />
+
+     <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+
+     <input type="file" name="mon_fichier" id="mon_fichier" /><br />
+
+     <input type="submit" name="submit" value="Envoyer" />
+
 </form>
+
+<?php
+} else {
+  $error = "";
+  if($_FILES['mon_fichier']['size'] > $maxsize){
+    $error = 'Le ficher est trop gros !';
+    $taille = 1;
+  }
+  if($_FILES['mon_fichier']['error'] > 0){
+    $error = 'Erreur lors du transfert !';
+    $erreur = 1;
+  }
+  $extension_upload = strtolower(  substr(  strrchr($_FILES['mon_fichier']['name'], '.')  ,1)  );
+  if(in_array($extension_upload,$extensions_valides)){
+    echo "Extension correcte";
+  } else {
+    $error = "L'extension est incorrecte !";
+    $extension = 1;
+  }
+  echo 'nom : '.$_FILES['mon_fichier']['name'];
+  echo '<br/>type : '.$_FILES['mon_fichier']['type'];
+  echo '<br/>taille : '.$_FILES['mon_fichier']['size'];
+  echo '<br/>fichier temporaire : '.$_FILES['mon_fichier']['tmp_name'];
+  echo '<br/>error : '.$_FILES['mon_fichier']['error'];
+  echo '<br/>'.$error;
+
+  if($taille == 0 && $extension == 0 && $erreur == 0){
+    $emplacement = $nomRepertoire.'test.pdf';
+    $resultat = move_uploaded_file($_FILES['mon_fichier']['tmp_name'],$emplacement);
+    if($resultat){
+      echo "Transfert réussi";
+    }
+  }
+}
+?>
