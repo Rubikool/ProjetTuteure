11297a4668fc89af3379006cf34a56980e596321
Zakuro Uzueda <zakuro.uzueda@hotmail.fr>
2016-01-14 09:19:29 +0100

    void

--- Site/include/pages/ChapitreLien.inc.php
+++
@@ -1,5 +0,0 @@
-<form method="post" action="#">
-<input type="hidden" name="tailleMax" value="1000000" />
-       <input type="file" name="mon_fichier" id="mon_fichier" /><br />
-<input type="submit" name="submit" value="Envoyer" />
-</form>
--- Site/include/pages/deconnexion.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$_SESSION["connecte"] = "";
-$_SESSION["pwd"] = "";
-$_SESSION["nom"] = "";
-
-header("Refresh: 0 ; URL = index.php");
-?>
